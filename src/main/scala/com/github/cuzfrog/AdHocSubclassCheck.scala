4a388b080111bf74d3babcbe2e6a92896fd88516
Cause Cheng <cuzfrog@139.com>
2017-07-25 10:16:01 +0800

    Add more

---
+++ src/main/scala/com/github/cuzfrog/AdHocSubclassCheck.scala
@@ -0,0 +1,39 @@
+package com.github.cuzfrog
+
+object AdHocSubclassCheck extends App {
+  trait SubClassGauge[A, B] {
+    def A_isSubclassOf_B: Boolean
+  }
+
+  implicit class IsSubclassOps[A](a: A) {
+    def isSubclassOf[B](implicit ev: SubClassGauge[A, B]): Boolean = ev.A_isSubclassOf_B
+  }
+
+  trait LowerLevelImplicits {
+    implicit def defaultSubClassGauge[A, B] = new SubClassGauge[A, B] {
+      override def A_isSubclassOf_B: Boolean = false
+    }
+  }
+
+  object Implicits extends LowerLevelImplicits {
+    implicit def subClassGauge[A <: B, B]: SubClassGauge[A, B] = new SubClassGauge[A, B] {
+      override def A_isSubclassOf_B: Boolean = true
+    }
+  }
+
+  trait Prime
+  class NotSuper
+  class Super
+  class Sub extends Super with Prime
+  class NotSub
+
+  import Implicits._
+
+  (new Sub).isSubclassOf[NotSuper]
+  (new Sub).isSubclassOf[Super]
+  (new Sub).isSubclassOf[Prime]
+  (new Super).isSubclassOf[Prime]
+  (new Super).isSubclassOf[Sub]
+  (new NotSub).isSubclassOf[Super]
+
+}
